6d89d2f6ea7612c9574b766da3c035c59ca2df6d
Shaijaan28 <shaijaanrauf@gmail.com>
2026-04-15 21:34:23 +0530

    Recursion - April 15

---
+++ Python/Basic.py
@@ -0,0 +1,51 @@
+
+print("shaijaan")
+name = "rauf"
+age = 23
+price = 58.36
+a= None
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(a))
+
+#logical operator
+#not
+print(not False)
+
+#and
+val1= True
+val2 = False
+print("ans operstor: ", val1 and val2)
+
+#inputs
+input("Shaijaan")
+
+name = input("Shaijaan")
+print("Welcome", name)
+
+#or
+print("ans operstor: ", val1 or val2)
+
+#Type Conversion
+a = 2
+b = 4.23
+sum = a+b
+print(sum)
+
+a = "2"
+b = 4.23
+sum = a+b
+print(sum)
+
+#Type casting
+a= int("7")
+b=9
+sum = a+b
+print(sum)
+
+#area of rec
+side = float(input("enter square side : "))
+print("area= ", side*side)
+
