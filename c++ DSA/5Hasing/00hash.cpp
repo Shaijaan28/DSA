9b31fbcf41b556420b15f9f440997c6df6c71dc4
Shaijaan28 <shaijaanrauf@gmail.com>
2026-04-28 23:27:24 +0530

    c++ DSA - April 28

---
+++ c++ DSA/5Hasing/00hash.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Custom hash (optional but useful in CP)
+struct custom_hash {
+    size_t operator()(long long x) const {
+        return x ^ (x >> 16);
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Hashing using unordered_map
+    unordered_map<int, int, custom_hash> freq;
+
+    // Count frequency
+    for(int i = 0; i < n; i++) {
+        freq[arr[i]]++;
+    }
+
+    // Print result
+    cout << "\nElement Frequencies:\n";
+    for(auto it : freq) {
+        cout << it.first << " -> " << it.second << endl;
+    }
+
+    return 0;
+}
