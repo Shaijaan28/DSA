db44a0cd9a7d8155c0c12a0fd5d5015ce85c83c8
Shaijaan28 <shaijaanrauf@gmail.com>
2026-05-06 23:33:22 +0530

    c++ DSA May 6

---
+++ Python/4 Dicti&Set/4set.py
@@ -0,0 +1,10 @@
+collection = {1,2,3,4,5, "rauf", "then", "rauf", 5}#ignore duplicate value
+# set - mutable
+# set - element - immutable
+
+print(collection)
+print(type(collection))
+
+#set methods
+
+#set.add()
