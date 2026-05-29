26a14a8a1f678bb1412f56e4bb08e0dd213c7603
Shaijaan28 <shaijaanrauf@gmail.com>
2026-05-29 23:41:32 +0530

    c++ DSA - May 29

--- c++ DSA/8 Binary Search/3bCountOccurrences.cpp
+++ c++ DSA/8 Binary Search/3bCountOccurrences.cpp
@@ -1,44 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-int lowerBound(vector<int> arr, int n, int x){
-    int low = 0, high = n-1;
-    int ans =n;
-    while(low<=high){
-        int mid = (low+high)/2;
-        if(arr[mid]>=x){
-            ans = mid;
-            high = mid -1;
-        }
-        else{
-            low = mid+1;
-        }
-    }
-    return ans;
-
-}
-int upperBound(vector<int> arr, int n, int x){
-    int low = 0, high = n-1;
-    int ans =n;
-    while(low<=high){
-        int mid = (low+high)/2;
-        if(arr[mid]>x){
-            ans = mid;
-            high = mid -1;
-        }
-        else{
-            low = mid+1;
-        }
-    }
-    return ans;
-}
-pair<int,int> firstAndLastPosition(vector<int> &arr, int n, int k){
-    int lb = lowerBound(arr,n,k);
-    if(lb==n||arr[lb]!=k) return {-1,-1};
-    return {lb,upperBound(arr,n,k)-1};
-    // complete the video......
-}
-
-// Without using lb and ub
 int firstOcurrence(vector<int> &arr, int n, int k){
     int low =0,high = n-1;
     int first =-1;
@@ -72,6 +33,11 @@ pair<int,int> firstAndLastPosition(vector<int> &arr, int n , int k){
     if(first==-1)return {-1,-1};
     int last = lastOccurrence(arr,n,k);
     return {first,last};
+}
+int count(vector<int>&arr, int n, int x){
+    pair<int, int> ans = firstAndLastPosition(arr,n,x);
+    if(ans.first == -1) return 0;
+    return ans.second - ans.first +1;
 }
 int main (){
     return 0;
