168c1b0739e028c56888b04c1deb5ae1225eb4fc
Shaijaan28 <shaijaanrauf@gmail.com>
2026-06-04 23:34:57 +0530

    c++ DSA - June 4

--- c++ DSA/7Array/35majorityElementNby3.cpp
+++ c++ DSA/7Array/35majorityElementNby3.cpp
@@ -1,18 +1,51 @@
 #include<bits/stdc++.h>
 using namespace std;
 //  better soln;
-vector<int> majorityElement(vector<int> v, int n){
-    vector<int> ls;
-    map<int,int> mpp;
-    int mini = (int) (n/3)+1;
+// vector<int> majorityElement(vector<int> v, int n){
+//     vector<int> ls;
+//     map<int,int> mpp;
+//     int mini = (int) (n/3)+1;
+//     for(int i=0;i<n;i++){
+//         mpp[v[i]]++;
+//         if(mpp[v[i]]==mini){
+//             ls.push_back(v[i]);
+//         }
+//         if(ls.size()==2)break;
+//     }
+//     sort(ls.begin(), ls.end());
+//     return ls;
+// }
+
+// optimal
+vector<int> majorityElement(vector<int> nums, int n){
+    int cnt1 =0,cnt2=0;
+    int el1= INT_MIN;
+    int el2 = INT_MIN;
     for(int i=0;i<n;i++){
-        mpp[v[i]]++;
-        if(mpp[v[i]]==mini){
-            ls.push_back(v[i]);
+        if(cnt1==0 && el2!=nums[i]){
+            cnt1 =1;
+            el1 =nums[i];
         }
-        if(ls.size()==2)break;
+        else if(cnt2 ==0 && el1!=nums[i]){
+            cnt2 =1;
+            el1 =nums[i];
+        }
+        else if(nums[i]==el1) cnt1++;
+        else if(nums[i]==el2) cnt2++;
+        else {
+            cnt1--, cnt2--;
+        }
     }
-    sort(ls.begin(), ls.end());
+    vector<int> ls;
+    cnt1=0,cnt2=0;
+    for(int i=0;i<n;i++){
+        if(el1==nums[i]) cnt1++;
+        if(el2 == nums[i]) cnt2++;
+    }
+    int mini = (int) n/3+1;
+    if(cnt1 >= mini) ls.push_back(el1);
+    if(cnt2>= mini) ls.push_back(el2);
+    sort(ls.begin(),ls.end());
     return ls;
 }
 int main (){
--- c++ DSA/8 Binary Search/12KokoEatingBananas.cpp
+++ c++ DSA/8 Binary Search/12KokoEatingBananas.cpp
@@ -1,5 +1,36 @@
-#include<iostream>
+#include<bits/stdc++.h>>
 using namespace std;
+int findMax(vector<int> &nums){
+    int maxi = INT_MIN;
+    int n = nums.size();
+    for(int i =0;i<n;i++){
+        maxi = max(maxi,nums[i]);
+    }
+    return maxi;
+}
+int calculateTotalHours(vector<int> &nums, int hourly){
+    int totalH =0;
+    int n = nums.size();
+    for(int i=0;i<n;i++){
+        totalH += ceil((double)nums[i]/(double)hourly);
+    }
+    return totalH;
+}
+int minimumRateToEatBananas(vector<int> nums, int h){
+    int low =1, high=findMax(nums);
+    while(low <= high){
+        int mid = (low+high)/2;
+        int totalH = calculateTotalHours(nums,mid);
+        if(totalH <= h){
+            high = mid -1;
+        }
+        else {
+            low = mid -1;
+        }
+    }
+    return low;
+
+}
 int main (){
     return 0;
 }
