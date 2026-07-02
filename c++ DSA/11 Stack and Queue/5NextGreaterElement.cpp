4da4383ad43b504f6568fcfc8c0e42c9d038b92a
Shaijaan28 <shaijaanrauf@gmail.com>
2026-07-02 22:57:09 +0530

    Stack - June 2

---
+++ c++ DSA/11 Stack and Queue/5NextGreaterElement.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> findNGE(vector<int> &arr) {
+    int n = arr.size();
+
+    vector<int> nge(n);
+    stack<int> st;
+
+    for (int i = n - 1; i >= 0; i--) {
+
+        while (!st.empty() && st.top() <= arr[i]) {
+            st.pop();
+        }
+
+        if (st.empty()) {
+            nge[i] = -1;
+        } else {
+            nge[i] = st.top();
+        }
+
+        st.push(arr[i]);
+    }
+
+    return nge;
+}
+
+int main() {
+    vector<int> arr = {4, 5, 2, 10, 8};
+
+    vector<int> ans = findNGE(arr);
+
+    for (int x : ans) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
---
+++ c++ DSA/11 Stack and Queue/7PrevSmallestEle.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> prevSmaller(vector<int>& arr) {
+
+        int n = arr.size();
+        vector<int> ans(n);
+
+        stack<int> st;
+
+        for(int i = 0; i < n; i++) {
+
+            while(!st.empty() && st.top() >= arr[i]) {
+                st.pop();
+            }
+
+            if(st.empty())
+                ans[i] = -1;
+            else
+                ans[i] = st.top();
+
+            st.push(arr[i]);
+        }
+
+        return ans;
+    }
+};
+int main (){
+    return 0;
+}
