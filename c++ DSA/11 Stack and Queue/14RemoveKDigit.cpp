a8ccbf3535b9f48f511762fdd5a906dd45d13831
Shaijaan28 <shaijaanrauf@gmail.com>
2026-07-04 23:04:47 +0530

    Stack - 4 July

---
+++ c++ DSA/11 Stack and Queue/14RemoveKDigit.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+ string removeKdigits(string num, int k) {
+        stack<char> st;
+
+        for (int i = 0; i < num.size(); i++) {
+            while (!st.empty() && k > 0 && st.top() > num[i]) {
+                st.pop();
+                k--;
+            }
+            st.push(num[i]);
+        }
+
+        while (k > 0 && !st.empty()) {
+            st.pop();
+            k--;
+        }
+
+        if (st.empty())
+            return "0";
+
+        string res = "";
+
+        while (!st.empty()) {
+            res += st.top();
+            st.pop();
+        }
+
+        reverse(res.begin(), res.end());
+
+        // Remove leading zeros
+        while (!res.empty() && res.front() == '0') {
+            res.erase(res.begin());
+        }
+
+        if (res.empty())
+            return "0";
+
+        return res;
+    }
+    int main (){
+        
+    }
