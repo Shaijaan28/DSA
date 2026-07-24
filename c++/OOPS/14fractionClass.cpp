67886e233576da5b0d278b179320d2088848f486
Shaijaan28 <shaijaanrauf@gmail.com>
2026-07-24 23:02:20 +0530

    OOPS - July 24

--- c++/OOPS/14fractionClass.cpp
+++ c++/OOPS/14fractionClass.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 class Fraction {
+public:    
     int num;
     int den;
     Fraction(int num , int den){
@@ -12,7 +13,9 @@ class Fraction {
     }
 };
 int main (){
-    // Fraction f1(1,2);
-    // Fraction f2(1,3);
+    Fraction f1(1,2);
+    Fraction f2(1,3);
+    f1.display();
+    f2.display();
 
 }
