4049b685db11905a84f25f76d0814b930c337634
Shaijaan28 <shaijaanrauf@gmail.com>
2026-08-04 22:40:55 +0530

    DSA Tree - August 4

--- c++ DSA/12 Trees/44DeleteBST.cpp
+++ c++ DSA/12 Trees/44DeleteBST.cpp
@@ -79,7 +79,7 @@ int main() {
     TreeNode* root = new TreeNode(5);
 
     root->left = new TreeNode(3);
-    root->right = new TreeNode(6);
+    root->right = new TreeNode(60);
 
     root->left->left = new TreeNode(2);
     root->left->right = new TreeNode(4);
